b9d24bf2ed4fc759414d6707c0a67f0cbe0814d2
Vishnu Teja Gunji <vishnu.pandu9848@gmail.com>
2025-08-17 13:12:43 +0530

    spark changes

--- dags/spark-pi.py
+++ dags/spark-pi.py
@@ -1,39 +1,18 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.operators.empty import EmptyOperator
-from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
 
 with DAG(
-    dag_id="spark_job_operator",
-    start_date=datetime(2025, 8, 16),
-    schedule_interval="@once",
+    dag_id="spark_operator_dag",
+    start_date=days_ago(1),
+    schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    start = EmptyOperator(task_id="start")
-
-    spark_submit = BashOperator(
-        task_id="spark_submit_task",
-        bash_command="""
-            /opt/bitnami/spark/bin/spark-submit \
-              --master k8s://https://kubernetes.default.svc \
-              --deploy-mode cluster \
-              --name demo-app \
-              --class demo.App \
-              --conf spark.executor.instances=2 \
-              --conf spark.kubernetes.namespace=spark-jobs \
-              --conf spark.kubernetes.container.image=bitnami/spark:3.2.4 \
-              --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
-              --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.jars.options.claimName=spark-jar-pvc \
-              --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.jars.mount.path=/mnt/jars \
-              --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.jars.options.claimName=spark-jar-pvc \
-              --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.jars.mount.path=/mnt/jars \
-              --conf spark.jars.ivy=/tmp/.ivy2 \
-              local:///mnt/jars/app.jar
-            """
+    submit_spark_app = SparkKubernetesOperator(
+        task_id="submit_spark_app",
+        namespace="spark-jobs",  # ✅ where SparkApplication CR should live
+        application_file="/opt/airflow/dags/spark-app.yaml",  # ✅ YAML inside Airflow's DAG folder
+        kubernetes_conn_id="kubernetes_default",
+        do_xcom_push=False,
     )
-
-    end = EmptyOperator(task_id="end")
-
-    start >> spark_submit >> end
